6f8bdc0a6aeadfe3da06cceb0ae1f52933ef2cdf
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-07-24 14:34:41 +0000

    update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "상명중 1-1",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
