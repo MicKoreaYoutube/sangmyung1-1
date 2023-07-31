2bae3b4b30fb2c5c6d4d74ee0f0d1b20dc35745e
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-07-31 08:47:05 +0000

    커밋 메시지

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "상명중 1-1",
+  name: "상명중 1-1 건의함",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
