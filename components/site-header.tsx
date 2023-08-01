8ebc9aba9c26a9da21c24a95be779bcc20b69501
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-01 06:21:38 +0000

    update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,28 +1,41 @@
 import Link from "next/link"
 
+import { LogAboutItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
-export function SiteHeader() {
+interface LogAboutProps {
+  items?: LogAboutItem[]
+}
+
+export function SiteHeader({ items }: LogAboutProps) {
+  items = siteConfig.logAbout.login
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.docs}
-              target="_blank"
-              rel="noreferrer"
-              className={buttonVariants()}
-            >
-              Documentation
-            </Link>
-            <ThemeToggle />
-          </nav>
+          {items?.length ? (
+            <nav className="flex gap-6">
+              {items?.map(
+                (item, index) =>
+                  item.href && (
+                    <Link
+                      key={index}
+                      href={item.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={buttonVariants()}
+                    >
+                      {item.title}
+                    </Link>
+                  )
+              )}
+            </nav>
+          ) : null}
         </div>
       </div>
     </header>
