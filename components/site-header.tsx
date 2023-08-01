fc90b162dfd061567c27a7d9285de40aad560f69
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-01 16:20:30 +0000

    update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,7 +13,7 @@ interface LogAboutProps {
 
 export function SiteHeader({ items }: LogAboutProps) {
   items = siteConfig.logAbout.login
-  const setVariant = [null, buttonVariants({ variant: "outline" })]
+  const setVariant = [buttonVariants(), buttonVariants({ variant: "outline" })]
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
