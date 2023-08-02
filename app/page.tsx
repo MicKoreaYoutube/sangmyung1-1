2cd982c2b7cbace39f41667d22a150b7aa08c504
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-02 09:58:24 +0000

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { buttonVariants } from "@/components/ui/button"
 export default function IndexPage() {
   return (
     <>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 mx-8">
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 mx-12">
         <div className="flex flex-col items-start gap-2">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
             Beautifully designed components <br className="hidden sm:inline" />
