caec0fd9ef3e03189c2f2c27e33a2773d1af735f
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-05 09:47:08 +0000

    z

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function IndexPage() {
         </div>
       </section>
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 bg-black dark:bg-white">
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <div className="flex max-w-[980px] flex-col items-start gap-2 text-white dark:text-black">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
             Beautifully designed components <br className="hidden sm:inline" />
             built with Radix UI and Tailwind CSS.
