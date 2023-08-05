a11845fa0a1d4d1fd5dd6d7775e28918347d7c0c
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-05 09:19:20 +0000

    ㄱㄱ

--- app/page.tsx
+++ app/page.tsx
@@ -37,13 +37,13 @@ export default function IndexPage() {
           <ThemeToggle />
         </div>
       </section>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 bg-black dark:bg-white">
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 bg-black dark:bg-white h-screen">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl text-white">
             Beautifully designed components <br className="hidden sm:inline" />
             built with Radix UI and Tailwind CSS.
           </h1>
-          <p className="max-w-[700px] text-lg text-muted-foreground">
+          <p className="max-w-[700px] text-lg text-white">
             Accessible and customizable components that you can copy and paste
             into your apps. Free. Open Source. And Next.js 13 Ready.
           </p>
