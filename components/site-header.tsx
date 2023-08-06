7a9a45c3b7e4edd34a2afddfde7abf06e06cb7c6
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-06 14:44:02 +0000

    다시 ㄱㄱ

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -58,7 +58,7 @@ export function SiteHeader({ items }: LogAboutProps) {
               <SheetHeader>
                 <SheetTitle>Edit profile</SheetTitle>
                 <SheetDescription>
-                  Make changes to your profile here. Click save when you're done.
+                  Make changes to your profile here. Click save when you&#39re done.
                 </SheetDescription>
               </SheetHeader>
               <div className="grid gap-4 py-4">
