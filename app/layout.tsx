e2da0107be2104f8c01bf74d1f5f327987b2318d
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-08 04:42:26 +0000

    되누?

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,27 @@ import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-TV0eMLTkNg8czZvLOUs-bhTCQcu98Qo",
+  authDomain: "sangmyung1-1.firebaseapp.com",
+  databaseURL: "https://sangmyung1-1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sangmyung1-1",
+  storageBucket: "sangmyung1-1.appspot.com",
+  messagingSenderId: "1074007264233",
+  appId: "1:1074007264233:web:1ed0bf73678459ae925978",
+  measurementId: "G-MTX2P9JZXK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
