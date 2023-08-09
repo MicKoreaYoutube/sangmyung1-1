cee2ccaf52b92f601577fb9ab69cf6381c02da4c
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 16:09:06 +0000

    safdf

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -16,8 +16,8 @@ export default function IndexPage() {
       <div className="relative h-[670px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <Image src="logo.png" alt="로고" height={50} width={50}/>
+          <div className="relative z-20 flex items-center text-lg font-medium font-KBO-Dia-Gothic_bold">
+            <Image src="logo.png" alt="로고" height={50} width={50} />
             상명중 1-1 건의함
           </div>
           <div className="relative z-20 mt-auto">
@@ -31,32 +31,36 @@ export default function IndexPage() {
         <div className="p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
+              <h1 className="text-2xl font-semibold tracking-tight font-KBO-Dia-Gothic_bold">
                 로그인
               </h1>
             </div>
-            <Input placeholder="아이디를 입력하세요."/>
-            <Input placeholder="비밀번호를 입력하세요."/>
-            <Button>로그인</Button>
+            <div className="font-SUITE-Regular">
+              <Input placeholder="아이디를 입력하세요." />
+              <Input placeholder="비밀번호를 입력하세요." />
+              <Button>로그인</Button>
+            </div>
             <hr />
-            <span className="px-8 text-center text-sm text-muted-foreground">계정이 없나요? <Link href="/auth/join" className="text-blue-500 hover:text-blue-700">회원가입→</Link></span>
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              로그인 버튼을 누르실 경우, 당신은 {" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                이용약관
-              </Link>{" "}
-              과{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                개인정보보호처리방침
-              </Link>
-              에 동의한 것으로 간주합니다.
-            </p>
+            <div className="font-SUITE-Regular">
+              <span className="px-8 text-center text-sm text-muted-foreground">계정이 없나요? <Link href="/auth/join" className="text-blue-500 hover:text-blue-700">회원가입→</Link></span>
+              <p className="px-8 text-center text-sm text-muted-foreground">
+                로그인 버튼을 누르실 경우, 당신은 {" "}
+                <Link
+                  href="/terms"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  이용약관
+                </Link>
+                과{" "}
+                <Link
+                  href="/privacy"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  개인정보보호처리방침
+                </Link>
+                에 동의한 것으로 간주합니다.
+              </p>
+            </div>
           </div>
         </div>
       </div>
