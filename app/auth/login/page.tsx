0417927a29b01532a492c3041db1f97842c2c7ac
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 00:51:18 +0000

    ㄴㅇㄻ

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -18,7 +18,7 @@ export default function IndexPage() {
         <div className="lg:p-8 flex-grow">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">로그인</h1>
+              <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
               <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
             </div>
             <div className="grid gap-6">
