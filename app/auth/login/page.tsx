1a39091b300cf693df53da5b2e91bb426da31fba
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 15:48:51 +0000

    ㄴㅁㅇㄹㄴㅇㄹ

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
+import Image from "next/image"
+import { getAuth } from "firebase/auth";
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export default function IndexPage() {
@@ -10,41 +13,18 @@ export default function IndexPage() {
     <>
       <div className="md:hidden">
       </div>
-      <div className="relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link>
+      <div className="relative h-[670px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
+            <Image src="logo.png" alt="로고" />
+            상명중 1-1 건의함
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                이 곳은 상명중 1학년 1반 건의함입니다! 우리반에 원하는 것들을 편하게 건의해주세요!
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
             </blockquote>
           </div>
         </div>
@@ -52,13 +32,12 @@ export default function IndexPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                로그인
               </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
             </div>
-            {/* <UserAuthForm /> */}
+            <Input placeholder="아이디를 입력하세요."/>
+            <Input placeholder="비밀번호를 입력하세요."/>
+            <Button>로그인</Button>
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
