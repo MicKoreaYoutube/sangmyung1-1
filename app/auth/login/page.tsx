a065fba873a6a09df9dc3bfbb2e759870ba16cd5
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 01:27:44 +0000

    fsda

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -15,7 +15,7 @@ export default function IndexPage() {
           </div>
         </div>
         <div className="p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-4 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">로그인</h1>
               <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
