cfc3720015f960eabf91e0c0ff9c3a946e47a8f6
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 16:03:07 +0000

    ㅁㄴㅇㄹㄴㅇㄹ

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -17,13 +17,13 @@ export default function IndexPage() {
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <Image src="logo.png" alt="로고" />
+            <Image src="logo.png" alt="로고" height={50} width={50}/>
             상명중 1-1 건의함
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                이 곳은 상명중 1학년 1반 건의함입니다! 우리반에 원하는 것들을 편하게 건의해주세요!
+                이 곳은 상명중 1학년 1반 건의함입니다!<br />우리반에 원하는 것들을 편하게 건의해주세요!
               </p>
             </blockquote>
           </div>
@@ -38,22 +38,24 @@ export default function IndexPage() {
             <Input placeholder="아이디를 입력하세요."/>
             <Input placeholder="비밀번호를 입력하세요."/>
             <Button>로그인</Button>
+            <hr />
+            <span className="px-8 text-center text-sm text-muted-foreground">계정이 없나요? <Link href="/auth/join" className="text-blue-500 hover:text-blue-700">회원가입→</Link></span>
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
+              로그인 버튼을 누르실 경우, 당신은 {" "}
               <Link
                 href="/terms"
                 className="underline underline-offset-4 hover:text-primary"
               >
-                Terms of Service
+                이용약관
               </Link>{" "}
-              and{" "}
+              과{" "}
               <Link
                 href="/privacy"
                 className="underline underline-offset-4 hover:text-primary"
               >
-                Privacy Policy
+                개인정보보호처리방침
               </Link>
-              .
+              에 동의한 것으로 간주합니다.
             </p>
           </div>
         </div>
