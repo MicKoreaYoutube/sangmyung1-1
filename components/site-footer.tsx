4c4ff942406aa36737d73b02fb29d0da1f5ae1e5
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 09:04:10 +0000

    sdffsd

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -23,7 +23,7 @@ import { Instagram } from "lucide-react"
 
 export function SiteFooter() {
     return (
-        <footer className="bg-white dark:bg-gray-900">
+        <footer className="bg-white dark:bg-gray-900 border-t">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
