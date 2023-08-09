a955d057becfea900a8dc6430502a541458cdb43
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 04:57:50 +0000

    sdfasdf

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -8,7 +8,9 @@ import { ThemeToggle } from "@/components/theme-toggle"
 export default function IndexPage() {
   return (
     <>
-      <div className="container relative h-screen max-h-[648px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="md:hidden">
+      </div>
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/examples/authentication"
           className={cn(
@@ -18,8 +20,9 @@ export default function IndexPage() {
         >
           Login
         </Link>
-        <div className="relative hidden h-screen flex-col bg-zinc-900 p-10 text-white dark:border-r lg:flex">
-          <div className="relative flex items-center text-lg font-medium">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -34,7 +37,7 @@ export default function IndexPage() {
             </svg>
             Acme Inc
           </div>
-          <div className="relative">
+          <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
                 &ldquo;This library has saved me countless hours of work and
@@ -45,7 +48,7 @@ export default function IndexPage() {
             </blockquote>
           </div>
         </div>
-        <div className="p-8">
+        <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
