3f8f4bbb178a485ca882b5c3b9146a1b8d7e8d06
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 01:32:45 +0000

    fdasfs

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -7,8 +7,8 @@ import { ThemeToggle } from "@/components/theme-toggle"
 export default function IndexPage() {
   return (
     <>
-      <div className="container relative min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900"></div>
           <div className="relative z-20 flex items-center text-lg font-medium">
             {/* 로고 넣는 곳 */}
