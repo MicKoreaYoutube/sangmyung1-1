1da80cbaed50f1d70efb944eb750400bb3417b15
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-09 15:25:52 +0000

    ㅁㄴㅇㄹ

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -28,13 +28,13 @@ export function SiteFooter() {
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link href="/" className="flex items-center">
-                            <img src="logo.png" className="h-8 mr-3" alt="sangmyung1-1 Logo" />
+                            <Image src="logo.png" className="h-8 mr-3" alt="sangmyung1-1 Logo" />
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">상명중1-1 건의함</span>
                         </Link>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">법적 관</h2>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">법 관련</h2>
                             <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
                                     <Link href="/informationPolicy" className="hover:underline hover:text-dark">개인정보보호처리방침</Link>
@@ -70,7 +70,8 @@ export function SiteFooter() {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                        ⓒ 2023 이준영. all rights reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
                         <Link
