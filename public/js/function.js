eac767aa9205d837cca1948a856c628d2b2122a2
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-10 07:59:11 +0000

    sdfdfsdf

--- public/js/function.js
+++ public/js/function.js
@@ -1,6 +1,6 @@
 export function displayError(method, msg) {
     if (method == "sangmyung") {
-        document.querySelector('#error').innerHTML = msg;
+        document.querySelector('#errorMessage').innerHTML = msg;
         document.querySelector('#error').classList.replace('hidden', 'inline-block')
         setTimeout(function () {
             document.querySelector('#error').classList.replace('inline-block', 'hidden')
