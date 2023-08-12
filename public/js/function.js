9256a1e033c7e35e5777101658e03552c281de63
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-12 01:19:34 +0000

    ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㄹ

--- public/js/function.js
+++ public/js/function.js
@@ -1,5 +1,3 @@
-'use client';
-
 export function displayError(msg) {
     document.querySelector('#errorMessage').innerHTML = msg;
     document.querySelector('#error').classList.replace('hidden', 'inline-block')
