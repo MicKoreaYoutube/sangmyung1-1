3f389b09668224b001b6794c0479e58021d98d91
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-12 00:40:22 +0000

    fuckyou

--- app/auth/logout/page.tsx
+++ app/auth/logout/page.tsx
@@ -1,4 +1,5 @@
 import { getAuth, signOut } from "firebase/auth";
+import { displayError } from "@/public/js/function"
 
 export default function IndexPage() {
   const auth = getAuth();
@@ -6,11 +7,14 @@ export default function IndexPage() {
   signOut(auth).then(() => {
     location.href = '/'
   }).catch((error) => {
+
   });
 
   return (
     <>
-      <span id="error">로그아웃 중...</span>
+      <div id="error" className="hidden">
+      <span id="errorMessage">로그아웃 중...</span>
+      </div>
     </>
   )
 }
