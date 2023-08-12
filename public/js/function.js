18988fa845423eda04991ad579338e5257da7863
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-12 01:21:08 +0000

    asdfdsfs

--- public/js/function.js
+++ public/js/function.js
@@ -1,7 +1,9 @@
 export function displayError(msg) {
-    document.querySelector('#errorMessage').innerHTML = msg;
-    document.querySelector('#error').classList.replace('hidden', 'inline-block')
-    setTimeout(function () {
-        document.querySelector('#error').classList.replace('inline-block', 'hidden')
-    }, 3000);
+    if (typeof window !== 'object') {
+        document.querySelector('#errorMessage').innerHTML = msg;
+        document.querySelector('#error').classList.replace('hidden', 'inline-block')
+        setTimeout(function () {
+            document.querySelector('#error').classList.replace('inline-block', 'hidden')
+        }, 3000);
+    }
 }
