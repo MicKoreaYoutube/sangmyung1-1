0e07a6c9eda6dde8a5467e636333232b2390a16c
MicKorea Youtube <mickoreayt@gmail.com>
2023-08-12 02:36:18 +0000

    sdfsdfdsf

--- app/auth/user/page.tsx
+++ app/auth/user/page.tsx
@@ -1,7 +1,104 @@
+'use client';
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+
 export default function IndexPage() {
     return (
         <>
-            <h1>Hallu</h1>
+            <Tabs defaultValue="account flex">
+                <TabsList className="flex flex-column w-64 flex-none">
+                    <TabsTrigger value="profile">프로필</TabsTrigger>
+                    <TabsTrigger value="email">이메일 등록</TabsTrigger>
+                    <TabsTrigger value="password">비밀번호 변경</TabsTrigger>
+                </TabsList>
+                <div className="flex-auto">
+                    <TabsContent value="profile">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Account</CardTitle>
+                                <CardDescription>
+                                    Make changes to your account here. Click save when you&#39;re done.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input id="name" defaultValue="Pedro Duarte" />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input id="username" defaultValue="@peduarte" />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Save changes</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                    <TabsContent value="email">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Password</CardTitle>
+                                <CardDescription>
+                                    Change your password here. After saving, you&#39;ll be logged out.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="current">Current password</Label>
+                                    <Input id="current" type="password" />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="new">New password</Label>
+                                    <Input id="new" type="password" />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Save password</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                    <TabsContent value="password">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Account</CardTitle>
+                                <CardDescription>
+                                    Make changes to your account here. Click save when you&#39;re done.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="space-y-2">
+                                <div className="space-y-1">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input id="name" defaultValue="Pedro Duarte" />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input id="username" defaultValue="@peduarte" />
+                                </div>
+                            </CardContent>
+                            <CardFooter>
+                                <Button>Save changes</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                </div>
+            </Tabs>
         </>
     )
 }
