ba5434b79f949b679541acf5e55023d36f52f2fb
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-13 05:28:26 +0000

    sdfdsfdsf

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -6,7 +6,7 @@ export default function IndexPage() {
     <>
       <section className="container grid items-center gap-6 my-28 max-w-[1000px] text-center place-element-center">
         <h1 className="KBO-Dia-Gothic_bold">이용약관</h1>
-        <p className="SUITE-Regular text-justify">
+        <p className="SUITE-Regular text-justify" style={{ whiteSpace: 'pre-line' }}>
           제 0장 정의
           1​. 관리자는 상명중학교 1학년 1반 2학기 학급 회장을 의미한다.
           2. 유저는 이 건의함을 이용하는 모든 사람을 의미한다.
