629ed7828a7caf48dd84c5469113ec446c9f66eb
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-14 14:14:54 +0000

    asfasf

--- app/board/suggestions/page.tsx
+++ app/board/suggestions/page.tsx
@@ -1,12 +1,15 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/public/js/firebase";
 
-export default async function IndexPage() {
+export default function IndexPage() {
+  async function fetchData () {
+    const querySnapshot = await getDocs(collection(db, "suggestions"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  }
 
-  const querySnapshot = await getDocs(collection(db, "suggestions"));
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
-  });
+  fetchData()
 
   return (
     <>
