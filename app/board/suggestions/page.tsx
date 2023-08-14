3505e5ba9a2394ca1743d8212729bd00916adfd1
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-14 14:48:16 +0000

    asdfdsfsd

--- app/board/suggestions/page.tsx
+++ app/board/suggestions/page.tsx
@@ -1,24 +1,20 @@
-'use clinet';
-
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/public/js/firebase";
 
 import { Button } from "@/components/ui/button";
 
-export default function IndexPage() {
-  async function fetchData () {
-    const querySnapshot = await getDocs(collection(db, "suggestions"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
-  }
-
-  fetchData()
+async function IndexPage() {
+  const querySnapshot = await getDocs(collection(db, "suggestions"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
 
   return (
     <>
       <h1>Hallu</h1>
-      <Button onClick={fetchData}>Hallu</Button>
+      <Button>Hallu</Button>
     </>
   )
 }
+
+export default IndexPage
