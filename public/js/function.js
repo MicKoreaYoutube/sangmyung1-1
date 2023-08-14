a2c1391accea28a32ac39509137b0a79645136e9
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-14 03:15:17 +0000

    sdfsdfsdfsdf

--- public/js/function.js
+++ public/js/function.js
@@ -14,6 +14,7 @@ export function logined() {
     const user = auth.currentUser;
 
     if (user) {
+    } else {
         if (typeof window !== 'undefined') {
             alert('이미 로그인 하셨습니다.')
             history.go(-1)
@@ -25,10 +26,10 @@ export function logouted() {
     const user = auth.currentUser;
 
     if (user) {
-    } else {
         if (typeof window !== 'undefined') {
             alert('로그인을 하셔야 접속 하실 수 있습니다.')
             history.go(-1)
         }
+    } else {
     }
 }
