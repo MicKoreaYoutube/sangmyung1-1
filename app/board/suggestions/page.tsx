33bb6267fcc9ad443b83c7a9e690beec932540ef
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-14 16:56:17 +0000

    sdfsfsdfdsf

--- app/board/suggestions/page.tsx
+++ app/board/suggestions/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { onSnapshot, collection, getDocs } from "firebase/firestore";
 import { db } from "@/public/js/firebase";
 import React, { useState, useEffect } from 'react';
