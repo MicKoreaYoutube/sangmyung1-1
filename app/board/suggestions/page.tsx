550a9bf65f1a652267309034e4aa276a161b1fc9
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-14 15:31:38 +0000

    asdfsdfsdf

--- app/board/suggestions/page.tsx
+++ app/board/suggestions/page.tsx
@@ -1,27 +1,16 @@
-import { doc, getDoc } from "firebase/firestore";
+import { onSnapshot, collection, getDocs } from "firebase/firestore";
 import { db } from "@/public/js/firebase";
+import React, { useState, useEffect } from 'react';
 
 import { Button } from "@/components/ui/button";
 
 export default function IndexPage() {
 
-  const docRef = doc(db, "suggestions", "8XyqHAA1pgbF1KcYOidR");
-
-  console.log(docRef)
-
-  async function fetchData() {
-
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      // docSnap.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  }
-
-  fetchData()
+  useEffect(() => {
+    onSnapshot(collection(db, "cities"), (snapshot) => {
+      console.log(snapshot);
+    })
+  })
 
   return (
     <>
