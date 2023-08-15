f5fd5a66b396160506ca420816ec6ec2f6549c20
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-15 06:09:31 +0000

    asdfsdf

--- app/board/suggestions/[suggestionID]/page.tsx
+++ app/board/suggestions/[suggestionID]/page.tsx
@@ -40,28 +40,38 @@ export default function IndexPage({
     }, []);
 
     return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Create project</CardTitle>
-                <CardDescription>Deploy your new project in one-click.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" placeholder="Name of your project" />
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="framework">Framework</Label>
-                        </div>
+        <>
+            <section className="container grid gap-6 my-28 max-w-[1000px] place-element-center">
+                {data ? (
+                    <div>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>{data.title}</CardTitle>
+                                <CardDescription>{data.author}·{new Date(data.changeTime).toLocaleString()}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <form>
+                                    <div className="grid w-full items-center gap-4">
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="name">Name</Label>
+                                            <Input id="name" placeholder="Name of your project" />
+                                        </div>
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="framework">Framework</Label>
+                                        </div>
+                                    </div>
+                                </form>
+                            </CardContent>
+                            <CardFooter className="flex justify-between">
+                                <Button variant="outline">Cancel</Button>
+                                <Button>Deploy</Button>
+                            </CardFooter>
+                        </Card>
                     </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <Button variant="outline">Cancel</Button>
-                <Button>Deploy</Button>
-            </CardFooter>
-        </Card>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </section>
+        </>
     )
 }
