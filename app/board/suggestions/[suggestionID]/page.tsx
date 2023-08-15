82c8b5123020bc5e0b0df943a51a7c44b275a00c
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-15 07:17:20 +0000

    asdfsfsdfsdfsdfsdfsdfdsfsdfsdsd

--- app/board/suggestions/[suggestionID]/page.tsx
+++ app/board/suggestions/[suggestionID]/page.tsx
@@ -64,7 +64,7 @@ export default function IndexPage({
 
     return (
         <>
-            <section className="container grid gap-6 my-28 max-w-[1000px] justify-content-between">
+            <section className="container grid gap-6 my-28 max-w-[1000px] content-start">
                 {data ? (
                     <div>
                         <Card>
