d5dfdbb346e0ec8c971d846c8f9b59c8ad51eaf7
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-15 04:42:27 +0000

    asdfsdfsdfsdfsdfdsfsdsdf

--- app/board/suggestions/page.tsx
+++ app/board/suggestions/page.tsx
@@ -40,8 +40,8 @@ export default function IndexPage() {
             {suggestions_list?.length ? (
               <nav className="nav-flex items-center space-x-2">
                 {suggestions_list.map((suggestion, index) => (
-                  <div key={index} className="items-center h-full">
-                    <h3 className="text-xl">{suggestion.title} ·<span className="text-sm text-gray-400">{suggestion.author}</span>·<span className="text-sm text-gray-400">{suggestion.changeTime.toLocaleString()}</span></h3>
+                  <div key={index} className="flex items-center h-full">
+                    <h3 className="text-xl text-center">{suggestion.title} ·<span className="text-sm text-gray-400">{suggestion.author}</span>·<span className="text-sm text-gray-400">{suggestion.changeTime.toLocaleString()}</span></h3>
                     <span className="text-lg text-gray-700">{suggestion.content.slice(0, 40)}...</span>
                     <Separator className="my-2 w-full" />
                   </div>
