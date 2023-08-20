aebb3b1d69defce5ca8e62d9763b8ad1c752f567
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-20 14:24:55 +0000

    asdfsdfsdf

--- app/board/suggestions/[suggestionID]/delete/page.tsx
+++ app/board/suggestions/[suggestionID]/delete/page.tsx
@@ -1,7 +1,75 @@
+'use client';
+
+import Link from "next/link"
+
+import { displayError } from "@/public/js/function";
+
+import { doc, updateDoc, Timestamp, collection, getDoc, getDocs } from "firebase/firestore";
+import { db } from "@/public/js/firebase";
+import React, { useRef, useState, useEffect } from 'react';
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 export default function IndexPage({ params }: { params: { suggestionID: string } }) {
+
+    const deleteData = useRef(null);
+
+    deleteData.current.click()
+
     return (
         <>
-            <h1>Hallu</h1>
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                    <Button variant="outline" ref={deleteData}>Show Dialog</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your
+                            account and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </>
     )
 }
