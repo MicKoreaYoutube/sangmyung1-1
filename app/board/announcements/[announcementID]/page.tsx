2cbe6c4ee8ae4aaf08a259581fd5d3892e39d915
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-25 07:58:33 +0000

    asdasdsad

--- app/board/announcements/[announcementID]/page.tsx
+++ app/board/announcements/[announcementID]/page.tsx
@@ -62,19 +62,21 @@ export default function IndexPage({ params }: { params: { announcementID: string
         <>
             <section className="container grid gap-6 my-28 max-w-[1000px]">
                 <Card className="justify-start">
+                    <div className="flex justify-end">
+                        <Link href="/board/announcements" className={buttonVariants({ variant: "ghost" }) + "font-SUITE-Regular px-2 absolute m-1"}><ChevronRight /></Link>
+                    </div>
                     {data ? (
                         <>
                             <CardHeader>
                                 <CardTitle className="font-KBO-Dia-Gothic_bold md:text-4xl flex justify-between">
                                     <span>{data.title}</span>
-                                    <Link href="/board/announcements" className={buttonVariants({ variant: "ghost" }) + "font-SUITE-Regular px-2"}><ChevronRight /></Link>
                                 </CardTitle>
                                 <CardDescription className="font-SUITE-Regular md:text-2xl">{data.author} · {formatTimestamp(data.changeTime)}</CardDescription>
                             </CardHeader>
                             <CardContent>
                                 <p className="text-lg font-SUITE-Regular whitespace-pre-wrap">{data.content}</p>
                             </CardContent>
-                          {/*<CardFooter className="font-SUITE-Regular flex flex-col justify-start w-full">
+                            {/*<CardFooter className="font-SUITE-Regular flex flex-col justify-start w-full">
                                 <Input placeholder="댓글을 달아보세요..." />
                                 {subcollectionData.map((item) => (
                                     <div key={item.id}>
