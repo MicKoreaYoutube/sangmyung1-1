8e7e546baf156c25fcca32534cf91c2f81687570
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-25 08:14:38 +0000

    ㅁㄴㅇㄹㄴㅇㄹㄴㅇ

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,30 @@
+'use client';
+
+import Link from "next/link"
+
+import { ChevronRight } from 'lucide-react';
+
+import { collection, doc, getDoc, getDocs, Timestamp } from "firebase/firestore";
+import { db } from "@/public/js/firebase";
+import React, { useState, useEffect } from 'react';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 export default function IndexPage() {
   return (
-    <h1>Hallu</h1>
+    <>
+      <h1>Hallu</h1>
+    </>
   )
 }
