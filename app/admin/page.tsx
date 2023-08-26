bc1a38040d482f2158279441b372d5b50244c448
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-26 05:09:17 +0000

    d

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,9 @@ import Link from "next/link"
 import { userInfo } from "@/public/js/firebase"
 import { accessDenied, displayError } from "@/public/js/function";
 
-import { collection, doc, getDoc, getDocs, Timestamp } from "firebase/firestore";
+import { siteConfig, SiteConfig } from "@/config/site";
+
+import { collection, setDoc, Timestamp, doc } from "firebase/firestore";
 import { db } from "@/public/js/firebase";
 import React, { useState, useEffect, useRef } from 'react';
 
@@ -30,6 +32,24 @@ import {
 export default function IndexPage() {
 
   const pwd = useRef(null)
+  const Temp = useRef(null)
+
+  const currentDate = new Date()
+
+  async function TempFunc(user: any) {
+    try {
+      const docRef = doc(db, "user", user);
+      await setDoc(docRef, {userBanStartTime: Timestamp.fromDate(currentDate), userBanEndTime: Timestamp.fromDate(currentDate)});
+      console.log("Document added or updated successfully!");
+    } catch (error) {
+      console.error("Error adding document:", error);
+    }
+  }
+
+  
+  const addUser = () => siteConfig.member.forEach((user) => {
+    TempFunc(user)
+  });
 
   setTimeout(function() {
     userInfo ? (
@@ -38,20 +58,24 @@ export default function IndexPage() {
   }, 500);
 
   function accessAdmin() {
-    pwd.current.value == "MTxgdTBrl59RGGKV8OtH" ? console.log("Success!") : displayError("옳지 않은 비밀번호입니다.")
+    pwd.current.value == "MTxgdTBrl59RGGKV8OtH" ? (
+      console.log("Success!")
+    ) : displayError("옳지 않은 비밀번호입니다.")
   }
   return (
     <>
       <Card className="place-element-center m-4">
-      <CardHeader>
-        <CardTitle>비밀번호 입력</CardTitle>
-        <CardDescription>관리자 페이지에 접근하려면 비밀번호를 입력하세요.</CardDescription>
-      </CardHeader>
-      <CardContent>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="pwd">비밀번호 입력</Label>
-              <Input placeholder="비밀번호를 입력해주세요..." ref={pwd} />
+        <CardHeader>
+          <CardTitle>비밀번호 입력</CardTitle>
+          <CardDescription>관리자 페이지에 접근하려면 비밀번호를 입력하세요.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col space-y-1.5">
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="pwd">비밀번호 입력</Label>
+                <Input placeholder="비밀번호를 입력해주세요..." ref={pwd} />
+              </div>
             </div>
           </div>
           <Alert variant="destructive" className="hidden" id="error">
@@ -60,11 +84,12 @@ export default function IndexPage() {
               Error Message
             </AlertDescription>
           </Alert>
-      </CardContent>
-      <CardFooter className="flex justify-end">
-        <Button onClick={accessAdmin}>접속하기</Button>
-      </CardFooter>
-    </Card>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button onClick={accessAdmin}>접속하기</Button>
+        </CardFooter>
+      </Card>
+      <Input ref={Temp} onClick={addUser}/>
     </>
   )
 }
