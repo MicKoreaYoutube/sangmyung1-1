483e76107f14de1823b0996764c508f4c5bbe719
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-26 04:11:47 +0000

    d

--- app/board/suggestions/page.tsx
+++ app/board/suggestions/page.tsx
@@ -69,7 +69,7 @@ export default function IndexPage() {
                   {suggestionsList.map((suggestion, index) => (
                     suggestion.status !== "delete" ? (
                       <>
-                        <div className="flex justify-between items-start" key={index}>
+                        <div className={`flex justify-between items-start ${index == 0 ? "ml-2" : null}`} key={index}>
                           <Link href={`/board/suggestions/${suggestion.id}`} className="hover:underline hover:underline-offset-2 w-full">
                             <h1 className="text-xl md:text-3xl font-KBO-Dia-Gothic_bold tracking-tighter inline-block">{suggestion.title}</h1>
                           </Link>
