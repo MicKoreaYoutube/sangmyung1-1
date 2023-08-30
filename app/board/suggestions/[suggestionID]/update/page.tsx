2946e6069b14f07fd803e6b8ac9e8922b2bf206f
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-30 18:03:18 +0000

    asdfsdfsdf

--- app/board/suggestions/[suggestionID]/update/page.tsx
+++ app/board/suggestions/[suggestionID]/update/page.tsx
@@ -95,7 +95,7 @@ export default function IndexPage({ params }: { params: { suggestionID: string }
     function formatTimestamp(timestamp: any) {
         if (timestamp !== null) {
             const dateObject = new Date(timestamp?.seconds * 1000);
-            return dateObject.toLocaleString()
+            return dateObject
         } else {
             return timestamp
         }
--- app/board/suggestions/create/page.tsx
+++ app/board/suggestions/create/page.tsx
@@ -187,8 +187,6 @@ export default function IndexPage() {
                         {userBanData ? (
                             <AlertDialogDescription className="font-SUITE-Regular">
                                 귀하는 이용약관 위반으로 현재 정지 상태이십니다.<br />
-                                정지 사유: <span>{userBanData.userBanReason}</span><br />
-                                정지 기간: <span>{formatTimestamp(userBanData.userBanStartTime)} ~ {userBanData.userBanEndTime == null ? "영구 정지" : userBanData.userBanEndTime}</span>
                                 {/* 정지 사유: <span>{userBanData.userBanReason}</span><br />
                             정지 기간: <span>{userBanData.userBanStartTime} ~ {userBanData.userBanEndTime == null ? "영구 정지" : userBanData.userBanEndTime}</span> */}
                             </AlertDialogDescription>
