bbcdac36fa6a8d2ef13d0fa0046ea3b64df8f46d
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-08-30 14:44:01 +0000

    ㅁㄴㅇㄹㄴㅇㄹㄴㅇㄹㄴㅇㄹ

--- app/board/suggestions/create/page.tsx
+++ app/board/suggestions/create/page.tsx
@@ -58,17 +58,21 @@ export default function IndexPage() {
     const userBanRange = useRef(null);
 
     const [userBanData, setUserBanData] = useState(null)
-    useEffect(()=>{
+    useEffect(() => {
         async function fetchSingleData() {
-            const cutEmail = userInfo.email.slice(0, 5)
-            const id = siteConfig.member.filter(item => item && item.toString().includes(cutEmail.toString()));
-    
-            const docRef = doc(db, "user", "19072김두한");
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                setUserBanData({ id: docSnap.id, ...docSnap.data() })
-                console.log(userBanData)
-            }
+            onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    const cutEmail = user.email.slice(0, 5)
+                    const id = siteConfig.member.filter(item => item && item.toString().includes(cutEmail.toString()));
+
+                    const docRef = doc(db, "user", "19072김두한");
+                    const docSnap = await getDoc(docRef);
+                    if (docSnap.exists()) {
+                        setUserBanData({ id: docSnap.id, ...docSnap.data() })
+                        console.log(userBanData)
+                    }
+                }
+            })
         }
         fetchSingleData();
     }, [])
@@ -86,31 +90,31 @@ export default function IndexPage() {
             userBanReason.current.innerHTML = data.userBanReason
             userBanRange.current.innerHTML = `${data.userBanStartTime} ~ ${data.userBanEndTime}`
         } else {*/
-            /*if (title.current.value == "" || content.current.innerHTML == "" || status.current.innerHTML == "익명 여부") {
-                displayError("모든 칸을 다 채워주세요.")
-            } else {*/
-                const collectionRef = collection(db, "suggestions");
-                onAuthStateChanged(auth, async (user) => {
-                    if (user) {
-                        const cutEmail = user.email.slice(0, 5)
-                        const id = siteConfig.member.filter(item => item && item.toString().includes(cutEmail.toString()));
-                        const currentDate = new Date();
-                        const status_list = { 공개: "all", 익명: "anonymous" }
-                        const statusValue: "공개" | "익명" = status.current.innerHTML
-                        const newData = { author: id[0], changeTime: Timestamp.fromDate(currentDate), content: content.current.value, status: status_list[statusValue], title: title.current.value, uploadTime: Timestamp.fromDate(currentDate) }
-                        try {
-                            const newDocRef = await addDoc(collectionRef, newData);
-                            const commentsCollection = collection(newDocRef, 'comments');
-                            const commentData = { status: "delete" };
-                            const newCommentDocRef = await addDoc(commentsCollection, commentData);
+        /*if (title.current.value == "" || content.current.innerHTML == "" || status.current.innerHTML == "익명 여부") {
+            displayError("모든 칸을 다 채워주세요.")
+        } else {*/
+        const collectionRef = collection(db, "suggestions");
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const cutEmail = user.email.slice(0, 5)
+                const id = siteConfig.member.filter(item => item && item.toString().includes(cutEmail.toString()));
+                const currentDate = new Date();
+                const status_list = { 공개: "all", 익명: "anonymous" }
+                const statusValue: "공개" | "익명" = status.current.innerHTML
+                const newData = { author: id[0], changeTime: Timestamp.fromDate(currentDate), content: content.current.value, status: status_list[statusValue], title: title.current.value, uploadTime: Timestamp.fromDate(currentDate) }
+                try {
+                    const newDocRef = await addDoc(collectionRef, newData);
+                    const commentsCollection = collection(newDocRef, 'comments');
+                    const commentData = { status: "delete" };
+                    const newCommentDocRef = await addDoc(commentsCollection, commentData);
 
-                            location.href = '/board/suggestions'
-                        } catch (error) {
-                            displayError(error)
-                        }
-                    }
-                });
-            //}
+                    location.href = '/board/suggestions'
+                } catch (error) {
+                    displayError(error)
+                }
+            }
+        });
+        //}
         //}
     }
 
